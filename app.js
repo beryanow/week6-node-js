76103220298bf009eb85f379f532777800c43e6a
Maxim Beryanov <33306395+beryanow@users.noreply.github.com>
2021-11-10 22:03:52 +0300

    test

--- app.js
+++ app.js
@@ -41,13 +41,7 @@ export default function appSrc(express, bodyParser, createReadStream, crypto, ht
     app.get('/test/', async (req, res) => {
         const URL = req.query.URL;
 
-        const browser = await puppeteer.launch({headless: true, args: ['--disable-gpu',
-                '--disable-dev-shm-usage',
-                '--disable-setuid-sandbox',
-                '--no-first-run',
-                '--no-sandbox',
-                '--no-zygote',
-                '--single-process']});
+        const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
         const page = await browser.newPage();
         await page.goto(URL);
 
