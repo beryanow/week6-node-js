ac04a880cf6063096f3ec78f71e6323379c019b2
Maxim Beryanov <33306395+beryanow@users.noreply.github.com>
2021-11-13 23:44:16 +0300

    test

--- app.js
+++ app.js
@@ -26,8 +26,6 @@ export default function appSrc(express, bodyParser, createReadStream, crypto, ht
 
     const app = express();
 
-    app.use(express.static('static'));
-
     app.use(bodyParser());
 
     app.use('/login/', (_, res) => {
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import fs from 'fs';
 import crypto from 'crypto';
 import http from 'http';
-import https from 'https';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import fetch from "node-fetch";
@@ -10,11 +9,6 @@ import pug from 'pug';
 import puppeteer from 'puppeteer';
 import appSrc from './app.js';
 
-const privateKey = fs.readFileSync('key.pem');
-const certificate = fs.readFileSync('cert.pem');
-
 const app = appSrc(express, bodyParser, fs.createReadStream, crypto, http, mongoose, fetch, pug, puppeteer);
 
-app.listen(80);
-
-https.createServer({key: privateKey, cert: certificate, passphrase: 'wow123'}, app).listen(443);
+app.listen(8080);
